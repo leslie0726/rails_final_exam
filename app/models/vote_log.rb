e1ceae5fbfdc5f8276ee77922585211bce8234ce
leslie_zou <event1511252@gmail.com>
2018-01-15 21:07:38 +0800

    忘記密碼功能新增&頁面調整
    忘記密碼功能新增&頁面調整

--- app/models/vote_log.rb
+++ app/models/vote_log.rb
@@ -1,3 +1,4 @@
 class VoteLog < ApplicationRecord
   belongs_to :candidate,counter_cache: true
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -46,10 +46,11 @@ ActiveRecord::Schema.define(version: 20180107024531) do
 
   create_table "vote_logs", force: :cascade do |t|
     t.integer "candidate_id"
-    t.string "user"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["candidate_id"], name: "index_vote_logs_on_candidate_id"
+    t.index ["user_id"], name: "index_vote_logs_on_user_id"
   end
 
 end
